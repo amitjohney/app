8a2e13a4689ff840e06b7fad65de61082d44517f
amit.kumar <amit.kumar@orange.com>
2024-01-05 19:29:54 +0530

    Added choices and excceptions

--- app.py
+++ app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_name():
+   
    color = os.environ.get('choice')
-   return f"<h1 style='color:{color}'>Hello world</h1>"
+   if color not in ["green","blue","black"]:
+     raise Exception("Failed")
+   else:
+     return f"<h1 style='color:{color}'>Hello world</h1>"
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0',port=5000)
