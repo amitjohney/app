ce12932f4ca47ad111d867fc2452d14674834766
amit.kumar <amit.kumar@orange.com>
2024-01-06 10:16:21 +0530

    Python Code Changes

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ import sys
 app = Flask(__name__)
 color = os.environ.get('choice')
 myfile=open("testoutput","a")
-myfile.write(f"choice:{color}")
+myfile.write(f"Environment variable value for choice is {color}")
 @app.route('/')
 def hello_name():
+   myfile=open("testoutput","a")
    myfile.write(f"input={request.args.get('input')}")
-   
+   myfile.close()
    if request.args.get('input') not in ["green","blue","black","red"]:
+     
      sys.exit(1)
    else:
      return f"<h1 style='color:{color}'>Hello world with Query String {request.args.get('input')}</h1>"
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0',port=5000)
-   myfile.close()
+   
