8955f589587c9e4b997f1d949c29c63f07de0ea3
amit.kumar <amit.kumar@orange.com>
2024-01-06 09:28:30 +0530

    Added another job

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask
 import os
 import sys
 app = Flask(__name__)
-
+myfile=open("testoutput","w")
 @app.route('/')
 def hello_name():
    
    color = os.environ.get('choice')
+   myfile.write(f"choice:{color}")
+   myfile.close()
    if color not in ["green","blue","black"]:
      sys.exit(1)
    else:
