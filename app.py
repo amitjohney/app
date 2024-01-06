70a87ed28ab685c38e30efb3af0963c706b8d2fd
amit.kumar <amit.kumar@orange.com>
2024-01-06 09:51:19 +0530

    Two jobs

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask,request
 import os
 import sys
 app = Flask(__name__)
-myfile=open("testoutput","w")
+color = os.environ.get('choice')
+myfile=open("testoutput","a")
+myfile.write(f"choice:{color}")
 @app.route('/')
 def hello_name():
-   
-   color = os.environ.get('choice')
-   myfile.write(f"choice:{color}")
+   myfile.write(f"input={request.args.get('input')}")
    myfile.close()
-   if color not in ["green","blue","black"]:
+   if request.args.get('input') not in ["green","blue","black","red"]:
      sys.exit(1)
    else:
-     return f"<h1 style='color:{color}'>Hello world</h1>"
+     return f"<h1 style='color:{color}'>Hello world with Query String {request.args.get('input')}</h1>"
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0',port=5000)
