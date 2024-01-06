8f1678f31b3524d4e0992f0c58b011a44613135c
amit.kumar <amit.kumar@orange.com>
2024-01-06 10:20:48 +0530

    Introduced a error

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 curl http://127.0.0.1:5000?input=black 
-curl http://127.0.0.1:5000?input=green
+curl http://127.0.0.1:5000?input=greens
 curl http://127.0.0.1:5000?input=red
 curl http://127.0.0.1:5000?input=blue
