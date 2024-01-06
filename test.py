d9ac5bb349c55153c5f714bd5a2826108a9936b6
amit.kumar <amit.kumar@orange.com>
2024-01-06 19:34:25 +0530

    workflow_modify

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 import requests
+import os
 
-for input in ["red","green","blue"]:
-  response = requests.get(f"http://127.0.0.1:5000?input={input}")
-  print(response)
+if os.environ.get("ENV_NAME") == "local":
+  for input in ["red","green","blue"]:
+    response = requests.get(f"http://127.0.0.1:5000?input={input}")
+    print(response)
+else:
+  for input in ["red","green","blue"]:
+    response = requests.get(f"https://amitapppod-service-amit-{os.environ.get('ENV_NAME')}.apps.opc-sonf-ogn.orange-guinee.com/?input={input}")
