ef397954ce7d7edf58eb23fa1706d507b0a3388c
amit.kumar <amit.kumar@orange.com>
2024-01-07 17:49:48 +0530

    electron

--- .github/workflows/prwork/entrypoint.sh
+++ .github/workflows/prwork/entrypoint.sh
@@ -5,4 +5,4 @@ REPO_OWNER=$2
 REPO_NAME=$3
 BUG_TITLE=$4
 BUG_MSG=$5
-curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/amitjohney/app/issues -d '{"title":"explainme","body":"$BUG_MSG","labels":["bug"]}'
+curl -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer $TOKEN" -H "X-GitHub-Api-Version: 2022-11-28" https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/issues -d "{\"title\":\"$BUG_TITLE\",\"body\":\"$BUG_MSG\",\"labels\":['bug']}"
